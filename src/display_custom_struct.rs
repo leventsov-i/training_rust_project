56ae2a0e9bae2e3b02a65d96ad2c6f8a014016a8
ivan.leventsov <leventsov.i@yandex.ru>
2020-10-19 21:13:58 +0300

    add display format for struct color

--- src/display_custom_struct.rs
+++ src/display_custom_struct.rs
@@ -1,9 +1,10 @@
 use std::fmt;
+use std::fmt::{Write, Formatter};
 
 pub(crate) struct List(Vec<i32>);
 
 impl List {
-    pub fn new(vec: Vec<i32>) -> List {
+    pub(crate) fn new(vec: Vec<i32>) -> List {
         List(vec)
     }
 }
@@ -23,7 +24,22 @@ impl fmt::Display for List {
     }
 }
 
-fn main() {
-    let v = List::new(vec![1, 2, 3]);
-    println!("{}", v);
+pub (crate) struct Color {
+    red: u8,
+    green: u8,
+    blue: u8
+}
+
+impl Color {
+    pub fn new(red: u8, green: u8, blue: u8) -> Self {
+        Color { red, green, blue }
+    }
+}
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "RGB ")?;
+        write!(f, "({}, {}, {}) ", self.red, self.green, self.blue)?;
+        write!(f, "{:#X}{:X}{:X}", self.red, self.green, self.blue)
+    }
 }
